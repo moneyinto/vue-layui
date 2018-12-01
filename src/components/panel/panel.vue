8e2512568eadfbd1425b615bc959adff39d23b7c
moneyinto <woyaoqianjin@gmail.com>
2018-12-01 11:31:50 +0800

    面板

--- src/components/panel/panel.vue
+++ src/components/panel/panel.vue
@@ -1,9 +1,17 @@
 <template>
-
+    <div class="layui-collapse" :lay-filter="`collapse_${_uid}`">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'Panel'
+        name: 'Panel',
+
+        mounted() {
+            this.$nextTick(() => {
+                this.$layui.element.render(`collapse(collapse_${this._uid})`);
+            });
+        }
     };
 </script>
