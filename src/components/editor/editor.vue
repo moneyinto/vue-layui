71f4b7a80a69dd1ddf54b870b5f8fc9c31b832f7
moneyinto <woyaoqianjin@gmail.com>
2018-12-04 16:20:32 +0800

    处理neditor上传图片配置问题

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -48,6 +48,8 @@ export default {
         insertScriptTag() {
             let editorScriptTag = document.getElementById('editorScriptTag');
             let configScriptTag = document.getElementById('configScriptTag');
+            let serviceScriptTag = document.getElementById('serviceScriptTag');
+            let s = null;
             // 如果这个tag不存在，则生成相关代码tag以加载代码
             if (editorScriptTag === null) {
                 configScriptTag = document.createElement('script');
@@ -58,7 +60,11 @@ export default {
                 editorScriptTag.type = 'text/javascript';
                 editorScriptTag.src = this.ueditorPath + 'neditor.all.js';
                 editorScriptTag.id = 'editorScriptTag';
-                let s = document.getElementsByTagName('head')[0];
+                serviceScriptTag = document.createElement('script');
+                serviceScriptTag.type = 'text/javascript';
+                serviceScriptTag.src = this.ueditorPath + 'neditor.service.js';
+                serviceScriptTag.id = 'serviceScriptTag';
+                s = document.getElementsByTagName('head')[0];
                 s.appendChild(configScriptTag);
                 s.appendChild(editorScriptTag);
             }
@@ -69,6 +75,7 @@ export default {
                 configScriptTag.addEventListener('load', () => {
                     this.scriptTagStatus++;
                     configScriptTag.loaded = true;
+                    if (this.scriptTagStatus === 2) s.appendChild(serviceScriptTag);
                     this.initEditor();
                 });
             }
@@ -78,6 +85,16 @@ export default {
                 editorScriptTag.addEventListener('load', () => {
                     this.scriptTagStatus++;
                     editorScriptTag.loaded = true;
+                    if (this.scriptTagStatus === 2) s.appendChild(serviceScriptTag);
+                    this.initEditor();
+                });
+            }
+            if (serviceScriptTag.loaded) {
+                this.scriptTagStatus++;
+            } else {
+                serviceScriptTag.addEventListener('load', () => {
+                    this.scriptTagStatus++;
+                    serviceScriptTag.loaded = true;
                     this.initEditor();
                 });
             }
@@ -85,7 +102,7 @@ export default {
         },
         initEditor() {
             // scriptTagStatus 为 2 的时候，说明两个必需引入的 js 文件都已经被引入，且加载完成
-            if (this.scriptTagStatus === 2 && this.instance === null) {
+            if (this.scriptTagStatus === 3 && this.instance === null) {
                 // Vue 异步执行 DOM 更新，这样一来代码执行到这里的时候可能 template 里面的 script 标签还没真正创建
                 // 所以，我们只能在 nextTick 里面初始化 UEditor
                 this.$nextTick(() => {
