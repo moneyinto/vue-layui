fd5cf13f73d2315c0a0b74be9569d457a2d532e4
moneyinto <woyaoqianjin@gmail.com>
2020-08-24 18:47:18 +0800

    处理table问题

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <table class="layui-hide" ref="table"></table>
-        <div v-if="isShow" style="display: none" ref="tableHtml">
-            <slot></slot>
+        <table class="layui-hide" :id="`table_${_uid}`" :lay-filter="`table_${_uid}`"></table>
+        <div v-if="isShow" style="display: none">
+            <div :id="`toolbar_${_uid}`">
+                <slot name="toolbar"></slot>
+            </div>
+            <div :id="`bar_${_uid}`">
+                <slot name="bar"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -32,16 +37,16 @@
                     const table = this.$layui.table;
                     const form = this.$layui.form;
                     let options = {
-                        elem: this.$refs.table
+                        elem: `#table_${self._uid}`
                     };
 
                     // 处理表格header中dom按钮
-                    if (this.options.toolbarDom) this.options.toolbar = this.$refs.tableHtml.querySelector(`[mref='${this.options.toolbarDom}']`);
+                    if (this.options.toolbarDom) options.toolbar = `#toolbar_${self._uid}`;
 
                     // 处理表格中的dom按钮
                     if (this.options.cols.length > 0) {
                         for (let col of this.options.cols[0]) {
-                            if (col.toolbarDom) col.toolbar = this.$refs.tableHtml.querySelector(`[mref='${col.toolbarDom}']`);
+                            if (col.toolbarDom) col.toolbar = `#${col.toolbarDom}_${self._uid}`;
                         }
                     }
 
@@ -50,46 +55,46 @@
                     this.isShow = false;
 
                     // 头工具栏事件
-                    table.on('toolbar', (obj) => {
+                    table.on(`toolbar(table_${self._uid})`, (obj) => {
                         this.$emit('table-toolbar-event', obj);
                     });
 
                     // 监听行工具事件
-                    table.on('tool', (obj) => {
+                    table.on(`tool(table_${self._uid})`, (obj) => {
                         this.$emit('table-bar-event', obj);
                     });
 
                     // 监听单元格编辑
-                    table.on('edit', (obj) => {
+                    table.on(`edit(table_${self._uid})`, (obj) => {
                         this.$emit('table-cell-edit-event', obj);
                     });
 
                     // 监听表格复选框选择
-                    table.on('checkbox', (obj) => {
+                    table.on(`checkbox(table_${self._uid})`, (obj) => {
                         this.$emit('table-checkbox-event', obj);
                     });
 
                     // 监听行单击事件
-                    table.on('row', (obj) => {
+                    table.on(`row(table_${self._uid})`, (obj) => {
                         this.$emit('table-row-event', obj);
                     });
 
                     // 监听行双击事件
-                    table.on('rowDouble', (obj) => {
+                    table.on(`rowDouble(table_${self._uid})`, (obj) => {
                         this.$emit('table-row-double-event', obj);
                     });
 
                     // 监听排序事件
-                    table.on('sort', (obj) => {
+                    table.on(`sort(table_${self._uid})`, (obj) => {
                         this.$emit('table-sort-event', obj);
                     });
 
                     // 监听表单事件
-                    form.on('switch', function(obj) {
+                    form.on(`switch(table_${self._uid})`, function(obj) {
                         self.$emit('table-form-switch-event', this, obj);
                     });
 
-                    form.on('checkbox', function(obj) {
+                    form.on(`checkbox(table_${self._uid})`, function(obj) {
                         self.$emit('table-form-checkbox-event', this, obj);
                     });
                 });
